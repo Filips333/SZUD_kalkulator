3d70447cfff1bb5a398e058454f829c5e3a365f3
Filips333 <99622349+Filips333@users.noreply.github.com>
2025-12-15 16:28:40 +0100

    Refaktorisanje koda i priprema za nove funkcije

--- src/main.c
+++ src/main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 
+// Funkcije kalkulatora
 // Funkcija za sabiranje
 int saberi(int a, int b) {
     return a + b;
 }
+
 int main() {
     printf("C Kalkulator\n");
     return 0;
