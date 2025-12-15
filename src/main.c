13d40343fd5a5d418707d36a51b92f4170afb1ae
Filips333 <99622349+Filips333@users.noreply.github.com>
2025-12-15 16:35:30 +0100

    Dodavanje koda u okviru main funkcije

--- src/main.c
+++ src/main.c
@@ -16,7 +16,39 @@ int oduzmi(int a, int b){
 int saberi(int a, int b) {
     return a + b;
 }
+
 int main() {
+    int izbor;
+    int x, y;
+
     printf("C Kalkulator\n");
+    printf("Izaberite operaciju:\n");
+    printf("1. Sabiranje\n");
+    printf("2. Oduzimanje\n");
+    printf("3. Mnozenje\n");
+    printf("4. Deljenje\n");
+    printf("Vas izbor: ");
+    scanf("%d", &izbor);
+
+    printf("Unesite dva broja: ");
+    scanf("%d %d", &x, &y);
+
+    switch(izbor) {
+        case 1:
+            printf("Rezultat: %d\n", saberi(x, y));
+            break;
+        case 2:
+            printf("Rezultat: %d\n", oduzmi(x, y));
+            break;
+        case 3:
+            printf("Rezultat: %d\n", mnozenje(x, y));
+            break;
+        case 4:
+            printf("Rezultat: %d\n", deljenje(x, y));
+            break;
+        default:
+            printf("Nepostojeca opcija!\n");
+    }
+
     return 0;
 }
